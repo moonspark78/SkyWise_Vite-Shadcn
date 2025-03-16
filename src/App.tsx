12482518daddda7ad4658e0e9f311f401b0d6521
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-16 18:22:01 +0100

    📈 git init  📈

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <div>
+      toto
+      <Button variant="destructive">add</Button>
+    </div>
+  )
+}
+
+export default App
