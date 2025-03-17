384508fcaa467817e4fa6d3ca74908ad1ad5346e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-17 22:19:11 +0100

    📈 Adding Layout in App  📈

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import './App.css'
 
 import { BrowserRouter } from 'react-router-dom'
+import Layout from './components/layout'
 
 function App() {
   
 
   return (
     <BrowserRouter>
+      <Layout>Hello</Layout>
     </BrowserRouter>
 
   )
