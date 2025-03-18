ab48a9b09083e25f219389959f5ee3209929aca3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-18 20:50:06 +0100

    📈 adding  ThemeProvider in the Layout  📈

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './App.css'
 
 import { BrowserRouter } from 'react-router-dom'
 import Layout from './components/layout'
+import { ThemeProvider } from './components/context/theme-provider'
 
 function App() {
   
 
   return (
     <BrowserRouter>
+    <ThemeProvider>
       <Layout>Hello</Layout>
+    </ThemeProvider>
     </BrowserRouter>
 
   )
