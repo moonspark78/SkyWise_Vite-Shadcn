7eac0635c5294d3427ab415d1597422308d99e91
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-21 19:06:06 +0100

    📈 working on the Header  📈

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
       <div>
         <Link to={"/"}>
-          <img src="/logo.png" alt="logo" className="h-14 "/>
+          <img src="/logo.png" alt="logo" className="h-14 rounded-full"/>
         </Link>
+
+        <div>
+          {/* search */}
+          {/* toggle */}
+        </div>
       </div>
     </header>
   )
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,6 +14,7 @@ const Layout = ({children}: PropsWithChildren) => {
           </div>
         </footer>
     </div>
+    
   )
 }
 
