5657cabe53f7a3bd730691e488edaff0a33e618e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-22 13:09:54 +0100

    📈 working on the Header Logo Theme  📈

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import { useTheme } from "./context/theme-provider"
+import { Moon, Sun } from "lucide-react"
 
 
 const Header = () => {
@@ -24,7 +25,11 @@ const Header = () => {
             className={`flex items-center cursor-pointer transition-transform duration-500 
               ${isDark ? "rotate-80" : "rotate-0"}`}
           >
-
+              {isDark ? (
+                <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all"/>
+              ) : (
+                <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all"/>
+              )}
           </div>
         </div>
       </div>
