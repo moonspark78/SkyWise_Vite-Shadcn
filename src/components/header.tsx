119d2daf736810b3c1cca9c9c2f90fc68e52423c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-22 12:41:49 +0100

    📈 working on the Header logo  📈

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom"
+import { useTheme } from "./context/theme-provider"
 
 
 const Header = () => {
+  const { theme } = useTheme()
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
-      <div>
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
-          <img src="/logo.png" alt="logo" className="h-14 rounded-full"/>
+          <img 
+            src={theme === "dark" ? "/logo.png" : "/logo1.png"} 
+            alt="logo" 
+            className="h-14 rounded-full"
+          />
         </Link>
 
         <div>
