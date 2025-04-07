778a8ee0db2bddfd96eda53daaa6933404485bb2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-07 19:29:14 +0200

    📈 working On the  WeatherDashboard  📈

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -8,8 +8,13 @@ const WeatherDashboard = () => {
       {/* Favorite Cities */}
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
-        <Button variant={"outline"}>
-          <RefreshCcw/>
+        <Button 
+          variant={"outline"} 
+          size={"icon"}
+          //onclick={handleRefresh}
+          //disabled={}
+          >
+          <RefreshCcw className="h-4 w-4"/>
         </Button>
       </div>
       {/* Current and Hourly weather */}
