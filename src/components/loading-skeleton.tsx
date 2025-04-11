b3915eb620b0697791d596a620a8fd3ecd117e4c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-11 21:38:32 +0200

    📈 working on  the WeatherSkeleton component  📈

--- src/components/loading-skeleton.tsx
+++ src/components/loading-skeleton.tsx
@@ -2,5 +2,14 @@ import { Skeleton } from "./ui/skeleton";
 
 function WeatherSkeleton() {
   return (
+    <div className="space-y-6">
+        <div className="grid gap-6">
+            <Skeleton className="h-[300px] w-full rounded-lg"/>
+            <Skeleton className="h-[300px] w-full rounded-lg"/>
+        </div>
+    </div>
+    
   );
 }
+
+export default WeatherSkeleton;
