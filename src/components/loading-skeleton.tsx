10c1203930c40a1ac8642e380de5379e7b1afed2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-11 21:40:08 +0200

    📈 working on  the weatherskeleton component  📈

--- src/components/loading-skeleton.tsx
+++ src/components/loading-skeleton.tsx
@@ -6,6 +6,10 @@ function WeatherSkeleton() {
         <div className="grid gap-6">
             <Skeleton className="h-[300px] w-full rounded-lg"/>
             <Skeleton className="h-[300px] w-full rounded-lg"/>
+            <div className="grid gap-6 md:grid-cols-2">
+                <Skeleton className="h-[300px] w-full rounded-lg"/>
+                <Skeleton className="h-[300px] w-full rounded-lg"/>
+            </div>
         </div>
     </div>
     
