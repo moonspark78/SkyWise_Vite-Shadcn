4f1c3764852ff74aa469a769ad8c66efc18292a1
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-12 10:19:34 +0200

    📈 working on  the weatherDashboard  📈

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,46 +1,56 @@
 import WeatherSkeleton from "@/components/loading-skeleton";
-import { Button } from "@/components/ui/button"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import useGeolocation from "@/hooks/use-geolocation";
-import { RefreshCcw } from "lucide-react"
-
+import { AlertCircle, RefreshCcw } from "lucide-react";
 
 const WeatherDashboard = () => {
-  const { coordinates, error: locationError, getLocation ,isLoading: locationLoading } = useGeolocation();
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
   console.log(coordinates);
 
-  const handleRefresh = () =>{
+  const handleRefresh = () => {
     getLocation();
-    if(coordinates){
+    if (coordinates) {
       //reload weather data
     }
   };
 
-  if(locationLoading){
-    return <WeatherSkeleton/>;
+  if (locationLoading) {
+    return <WeatherSkeleton />;
   }
 
-  if(locationError){
-
+  if (locationError) {
+    <Alert variant="destructive">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Location Error</AlertTitle>
+      <AlertDescription>
+        <p>{locationError}</p>
+      </AlertDescription>
+    </Alert>;
   }
-  
-  
+
   return (
     <div className="space-y-4">
       {/* Favorite Cities */}
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
-        <Button 
-          variant={"outline"} 
+        <Button
+          variant={"outline"}
           size={"icon"}
           onClick={handleRefresh}
           //disabled={}
-          >
-          <RefreshCcw className="h-4 w-4"/>
+        >
+          <RefreshCcw className="h-4 w-4" />
         </Button>
       </div>
       {/* Current and Hourly weather */}
     </div>
-  )
-}
+  );
+};
 
-export default WeatherDashboard
+export default WeatherDashboard;
