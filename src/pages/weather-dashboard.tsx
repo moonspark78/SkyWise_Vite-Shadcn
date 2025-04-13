b2260d8bbad94915cbc62312c7b9b9587b5ac183
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-13 20:39:43 +0200

    📈 working On  the WeatherDashboard  📈

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -2,7 +2,7 @@ import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import useGeolocation from "@/hooks/use-geolocation";
-import { AlertCircle, MapPin, RefreshCcw } from "lucide-react";
+import {  AlertTriangle, MapPin, RefreshCcw } from "lucide-react";
 
 const WeatherDashboard = () => {
   const {
@@ -27,10 +27,25 @@ const WeatherDashboard = () => {
   if (locationError) {
     return(
       <Alert variant="destructive">
-      <AlertCircle className="h-4 w-4" />
+      <AlertTriangle className="h-4 w-4" />
       <AlertTitle>Location Error</AlertTitle>
       <AlertDescription className="flex flex-col gap-4">
         <p>{locationError}</p>
+        <Button onClick={getLocation} variant={"outline"} className="w-fit">
+          <MapPin className="mt-2 h-4 w-4" />
+          Enable Location
+        </Button>
+      </AlertDescription>
+    </Alert>
+    )
+  }
+
+  if (!coordinates) {
+    return(
+      <Alert variant="destructive">
+      <AlertTitle>Location Required</AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+        <p>Please Enable location acces to see your local weather.</p>
         <Button onClick={getLocation} variant={"outline"} className="w-fit">
           <MapPin className="mt-2 h-4 w-4" />
           Enable Location
