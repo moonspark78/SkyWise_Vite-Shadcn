ff7fc3fa8b391e173793ad10124cf43e145087d9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-14 19:44:09 +0200

    📈 create the useWeatherQuery  Components 📈

--- src/hooks/use-weather.ts
+++ src/hooks/use-weather.ts
@@ -1,4 +1,9 @@
 import { Coordinates } from "@/api/types";
+import { useQuery } from "@tanstack/react-query";
 
 
-export function useWeatherQuery(coordinates: Coordinates | null){}
+export function useWeatherQuery(coordinates: Coordinates | null){
+    useQuery({
+        queryKey: ,
+    })
+}
