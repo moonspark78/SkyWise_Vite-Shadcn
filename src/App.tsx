cde9a43858176a8e0e695679ec47de5c329731f1
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-14 20:01:38 +0200

    📈 create the useWeatherQuery components 📈

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient({
   defaultOptions:{
     queries:{
-      
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
     }
   }
 })
