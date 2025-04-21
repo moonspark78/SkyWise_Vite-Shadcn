f8c9902610f73dab6e9e10215dbd594b769c2d81
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-21 18:52:26 +0200

    Card from shadCn

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,14 +1,27 @@
-import { GeocodingResponse, WeatherData } from "@/api/types"
+import { GeocodingResponse, WeatherData } from "@/api/types";
+import { CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 
 interface CurrentWeatherProps {
-    data: WeatherData;
-    locationName?: GeocodingResponse;
+  data: WeatherData;
+  locationName?: GeocodingResponse;
 }
 
-const CurrentWeather = ({data, locationName}: CurrentWeatherProps) => {
+const CurrentWeather = ({ data, locationName }: CurrentWeatherProps) => {
+  
   return (
-    <div>CurrentWeather</div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
