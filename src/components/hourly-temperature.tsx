ee7dd7cb0f54a652c66f9b5523911b3bf55e122e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-30 21:02:16 +0200

    working On The CurrentWeather

--- src/components/hourly-temperature.tsx
+++ src/components/hourly-temperature.tsx
@@ -1,15 +1,21 @@
 import type { ForecastData } from "@/api/types";
-
-
+import { Card } from "./ui/card";
 
 interface HourlyTemperatureProps {
-    data: ForecastData;
-} 
-
-const HourlyTemperature = ({data}: HourlyTemperatureProps) => {
-  return (
-    <div>HourlyTemperature</div>
-  )
+  data: ForecastData;
 }
 
-export default HourlyTemperature
+const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default HourlyTemperature;
