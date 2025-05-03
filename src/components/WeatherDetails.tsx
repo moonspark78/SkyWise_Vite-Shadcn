071c500ccd3203d67b137a3d019a17fde0b74e47
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-04 01:16:58 +0200

    :bookmark: working on  The WeatherDetails components  :bookmark:

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -1,10 +1,23 @@
 import type { WeatherData } from "@/api/types"
+import { format } from "path";
+import { title } from "process";
 
 interface WeatherDetailsProps {
     data: WeatherData
 }
 
 const WeatherDetails = ({data}: WeatherDetailsProps) => {
+    const { main, wind, sys } = data;
+
+    const details =[
+        {
+            title: "Sunrise",
+            value: formatTime(sys.sunrise),
+            icon: Sunrise,
+            color: "text-orange-500"
+        }
+    ]
+
   return (
     <div>WeatherDetails</div>
   )
