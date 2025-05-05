6dd7d22b12073a0024cbb7df5d22d194bd786323
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-06 00:24:41 +0200

    :bookmark: working on the  the WeatherForcast Components  :bookmark:

--- src/components/WeatherForcast.tsx
+++ src/components/WeatherForcast.tsx
@@ -1,4 +1,5 @@
 import type { ForecastData } from "@/api/types";
+import { format } from "date-fns";
 
 
 interface WeatherForcastProps {
@@ -6,6 +7,13 @@ interface WeatherForcastProps {
 }
 
 const WeatherForcast = ({data}: WeatherForcastProps) => {
+
+
+    const dailyForcast = data.list.reduce((acc,forecast) =>{
+        const date = format(new Date(forecast.dt * 1000), 'yyyy-MM-dd');
+    }, {})
+
+    
   return (
     <div>WeatherForcast</div>
   )
