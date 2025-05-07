c6a9cb215cf9e89c2d6b80a124c7da1d84f4288d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-07 08:07:52 +0200

    :bookmark: working on the  the Weather-Forcast Components  :bookmark:

--- src/components/WeatherForcast.tsx
+++ src/components/WeatherForcast.tsx
@@ -1,37 +1,59 @@
 import type { ForecastData } from "@/api/types";
 import { format } from "date-fns";
 
-
 interface WeatherForcastProps {
   data: ForecastData;
 }
 
-const WeatherForcast = ({data}: WeatherForcastProps) => {
-
-
-    const dailyForcast = data.list.reduce((acc,forecast) =>{
-        const date = format(new Date(forecast.dt * 1000), 'yyyy-MM-dd');
-
-        if(!acc[date]){
-            acc[date] = {
-                temp_min: forecast.main.temp_min,
-                temp_max: forecast.main.temp_max,
-                humidity: forecast.main.humidity,
-                wind: forecast.wind.speed,
-                weather: forecast.weather[0],
-                date: forecast.dt,
-            };
-        }else {
-            acc[date].temp_min = Math.min(acc[date].temp_min, forecast.main.temp_min);
-            acc[date].temp_max = Math.max(acc[date].temp_max, forecast.main.temp_max);
-            
-        }
-    }, {})
-
-    
-  return (
-    <div>WeatherForcast</div>
-  )
+interface DailyForecast {
+  date: number;
+  temp_min: number;
+  temp_max: number;
+  humidity: number;
+  wind: number;
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  };
 }
 
-export default WeatherForcast
+const WeatherForcast = ({ data }: WeatherForcastProps) => {
+  const dailyForcast = data.list.reduce((acc, forecast) => {
+    const date = format(new Date(forecast.dt * 1000), "yyyy-MM-dd");
+
+    if (!acc[date]) {
+      acc[date] = {
+        temp_min: forecast.main.temp_min,
+        temp_max: forecast.main.temp_max,
+        humidity: forecast.main.humidity,
+        wind: forecast.wind.speed,
+        weather: forecast.weather[0],
+        date: forecast.dt,
+      };
+    } else {
+      acc[date].temp_min = Math.min(acc[date].temp_min, forecast.main.temp_min);
+      acc[date].temp_max = Math.max(acc[date].temp_max, forecast.main.temp_max);
+    }
+    return acc;
+  }, {} as Record<string, DailyForecast>);
+  const nextDays = Object.values(dailyForcast).slice(0, 6);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default WeatherForcast;
