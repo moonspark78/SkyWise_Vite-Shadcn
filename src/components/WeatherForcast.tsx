6b30cb2b456bdeb93fb5339a9ec7594f81c1dc15
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-08 12:35:01 +0200

    :bookmark: working on The  Weather-Forcast Components  :bookmark:

--- src/components/WeatherForcast.tsx
+++ src/components/WeatherForcast.tsx
@@ -1,7 +1,7 @@
 import type { ForecastData } from "@/api/types";
 import { format } from "date-fns";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, Droplet, Wind } from "lucide-react";
 
 interface WeatherForcastProps {
   data: ForecastData;
@@ -73,7 +73,16 @@ const WeatherForcast = ({ data }: WeatherForcastProps) => {
                         </span>
                     </div>
 
-                    <div></div>
+                    <div className="flex justify-end gap-4">
+                        <span className="flex items-center gap-1">
+                          <Droplet className="h-4 w-4 text-blue-500"/>
+                          <span className="text-sm">{day.humidity}%</span>
+                        </span>
+                        <span className="flex items-center gap-1">
+                          <Wind className="h-4 w-4 text-blue-500"/>
+                          <span className="text-sm">{day.wind} m/s</span>
+                        </span>
+                    </div>
                 </div>
             );
             })}
