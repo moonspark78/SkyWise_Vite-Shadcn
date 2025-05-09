9f051bbc01603f9850c09fb8f6cb964ed6c4a3ff
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-09 08:03:09 +0200

    :bookmark: workin on  the CitySearch Components  :bookmark:

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -1,9 +1,27 @@
+import { Button } from "./ui/button";
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "./ui/command";
+
 
 
 const CitySearch = () => {
   return (
-    <div>CitySearch</div>
-  )
-}
+    <>
 
-export default CitySearch
+    <Button>Search cities ...</Button>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+
+export default CitySearch;
