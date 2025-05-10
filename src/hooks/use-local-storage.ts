e5b77253bc8914eb57da3c7546b3089bfd5a23d8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-11 00:28:16 +0200

    :bookmark: working on the   useLocalStorage components  :bookmark:

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -13,6 +13,12 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     });
 
     useEffect(() => {
-
+        try{
+            window.localStorage.setItem(key, JSON.stringify(storedValue));
+        }catch(error) {
+            console.error(error);
+        };
     }, [key, storedValue]);
+
+    return [storedValue, setStoredValue] as const;
 }
