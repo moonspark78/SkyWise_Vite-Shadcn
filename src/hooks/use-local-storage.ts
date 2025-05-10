10bce4b8532626d51da2108c4a08dd4513c168e6
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-11 00:23:31 +0200

    :bookmark: create useLocal-Storage components  :bookmark:

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -2,5 +2,10 @@ import { useState } from "react";
 
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
-    const [storedValue, setStoredValue] = useState<T>(() => {});
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        try{
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        }
+    });
 }
