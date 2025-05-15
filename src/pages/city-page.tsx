e99d9ac3852a115b45ba7276d078605cb7744f74
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-16 00:08:38 +0200

    :bookmark: working on the  CitySearch  Components  :bookmark:

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,8 +1,10 @@
-import { useSearchParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 
 const CityPage = () => {
   const [searchParams] = useSearchParams();
+  const params = useParams();
+  
   return (
     <div>CityPage</div>
   )
