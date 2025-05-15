eb7e20b28bca9067f12ad0fa8e4ef6eac9e5e17f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-16 00:09:25 +0200

    :bookmark: working on the  City-Search  Components  :bookmark:

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -4,7 +4,8 @@ import { useParams, useSearchParams } from "react-router-dom";
 const CityPage = () => {
   const [searchParams] = useSearchParams();
   const params = useParams();
-  
+  const lat = parseFloat(searchParams.get("lat") || "0");
+
   return (
     <div>CityPage</div>
   )
