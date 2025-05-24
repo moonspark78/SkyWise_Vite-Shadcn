0fff64e0bfbc25002a1b3f9ade9feb4d025d0b40
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-24 17:20:07 +0200

    📈  working on the FavoritesButton  componantes 📈

--- src/components/favorite-button.tsx
+++ src/components/favorite-button.tsx
@@ -6,6 +6,10 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton = ({data}: FavoriteButtonProps) => {
+    const { addFavorite, removeFavorite, isFavorite } = useFavorites();
+    const isCurrentlyFavorite = isFavorite(data.coord.lat, data.coord.lon);
+
+    
   return (
     <div>favorite-button</div>
   )
