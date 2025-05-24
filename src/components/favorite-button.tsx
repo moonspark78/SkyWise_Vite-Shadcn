da4399e672e70e497f8e918ac5f6d5b7a135c656
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-24 17:14:45 +0200

    📈  create the FavoriteButton componantes 📈

---
+++ src/components/favorite-button.tsx
@@ -0,0 +1,14 @@
+import { WeatherData } from "@/api/types"
+
+
+interface FavoriteButtonProps {
+    data: WeatherData
+}
+
+const FavoriteButton = ({data}: FavoriteButtonProps) => {
+  return (
+    <div>favorite-button</div>
+  )
+}
+
+export default FavoriteButton
