cf0146248bad073c47baaca6e2924730cad78b96
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-31 12:54:49 +0200

    📈  working on the Favorite-Cities  componantes 📈

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -6,6 +6,7 @@ import { useSearchLocations } from "@/hooks/use-weather";
 import { useNavigate } from "react-router-dom";
 import { useSearchHistory } from "@/hooks/use-search-history";
 import { format } from "date-fns";
+import { useFavorites } from "@/hooks/use-favorites";
 
 
 
@@ -33,7 +34,7 @@ const CitySearch = () => {
     navigate(`/city/${name}?lat=${lat}&lon=${lon}`)
   };
 
-
+  const {favorites} = useFavorites();
 
 
   return (
@@ -54,11 +55,51 @@ const CitySearch = () => {
           {query.length>2 && !isLoading && (
             <CommandEmpty>No results found.</CommandEmpty>
           )}
-          {/* <CommandGroup heading="Favorites">
-            <CommandItem>Calendar</CommandItem>
-          </CommandGroup> */}
 
+          {favorites.length > 0 && (
+            <>
+            <CommandSeparator/>
+            <CommandGroup>
+              <div className="flex items-center justify-between px-2 my-2">
+                <p className="text-xs text-muted-foreground">Recent Searches</p>
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => clearHistory.mutate()}
+                >
+                  <XCircle className="h-4 w-4"/>
+                  Clear
+                </Button>
+              </div>
+              {history.map((location) => {
+                 return (
+                  <CommandItem
+                    key={`${location.lat}-${location.lon}`}
+                    value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                    onSelect={handleSelect}
+                  >
+                    <Clock className="mr-2 h-4 w-4 text-muted-foreground"/>
+                    <span>{location.name}</span>
+                    {location.state && (
+                      <span className="text-sm text-muted-foreground">
+                        , {location.state}
+                      </span>
+                    )}
+                    <span className="text-sm text-muted-foreground">
+                      , {location.country}
+                    </span>
+                    <span>
+                      {format(location.searchedAt, "MMM d, h:mm a")}
+                    </span>
+                  </CommandItem>
+                 )
+              })}
+            </CommandGroup>
+            </>
+          )}
+          
 
+{/* ----------------------------------------------------------------------------------------- */}
           {history.length > 0 && (
             <>
             <CommandSeparator/>
